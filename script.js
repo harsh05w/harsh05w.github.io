52b89fd95dbc802b8a14bad41b205be35ad3f633
Harshwardhan Jadhav <138810884+harsh05w@users.noreply.github.com>
2025-06-06 23:02:48 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".multiple-text", {
-    strings: ["Video Editor", "Graphics Designer","Web Developer", "Data Entry Operator", "Content Creator"],
-    typeSpeed: 50,
-    backSpeed: 50,
-    backDelay: 1000,
-    loop: true
-})
